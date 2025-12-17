45ee08d3a0858a80b07a57d8c9ce82ac107eb063
ponaalagar <ponaalagarok@gmail.com>
2025-12-17 10:27:01 +0530

    coded till put

--- node_tut/app.js
+++ node_tut/app.js
@@ -3,10 +3,42 @@ import express from "express";
 
 const app = express();
 
-app.get("/hello",(req,res)=>{
-    res.send("<h1>vanakkam da mapla</h1>")
+const message = "vanakkam da mapla"
+app.use(express.json());
+
+
+app.get("/get", (req, res) => {
+    res.send(message)
+    console.log("success")
 })
 
-app.listen(3000, () =>{
+app.post("/post", (req, res) => {
+    const {name,college,role}= req.body
+    let message = "vanakkam " + name + " from " + college + " as " + role
+    console.log(name,college,role)
+    res.send(message)
+})
+
+app.put("/user/:id", (req, res) => {
+  const userId = req.params.id;   // URL param
+  const updatedData = req.body;   // body data
+
+  console.log("User ID:", userId);
+  console.log("Updated Data:", updatedData);
+
+  res.json({
+    success: true,
+    message: "User updated successfully",
+    userId,
+    updatedData
+  });
+});
+
+app.delete("/delete", (req, res) => {
+    res.send("deleted")
+    console.log("deleted")
+})
+
+app.listen(3000, () => {
     console.log("server running on port 3000")
 })
